f849bcda3779e5b45482ea5da3c7e6c0e133bbb1
ArthurAgustinho <130577092+ArthurAgustinho@users.noreply.github.com>
2024-10-06 23:14:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const botoes1 = document.querySelectorAll(".botoes1 .botao");
+const textos1 = document.querySelectorAll(".abas-textos1 .aba-conteudo");
+
+for (let i = 0; i < botoes1.length; i++) {
+    botoes1[i].onclick = function () {
+        for (let j = 0; j < botoes1.length; j++) {
+            botoes1[j].classList.remove("ativo");
+            textos1[j].classList.remove("ativo");
+        }
+        botoes1[i].classList.add("ativo");
+        textos1[i].classList.add("ativo");
+    }
+}
+
+const botoes2 = document.querySelectorAll(".botoes2 .botao");
+const textos2 = document.querySelectorAll(".abas-textos2 .aba-conteudo");
+
+for (let i = 0; i < botoes2.length; i++) {
+    botoes2[i].onclick = function () {
+        for (let j = 0; j < botoes2.length; j++) {
+            botoes2[j].classList.remove("ativo");
+            textos2[j].classList.remove("ativo");
+        }
+        botoes2[i].classList.add("ativo");
+        textos2[i].classList.add("ativo");
+    }
+}''
