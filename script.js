f8fb2d028a02c63ba550b65bc37d65f61abb063a
ArthurAgustinho <130577092+ArthurAgustinho@users.noreply.github.com>
2024-10-08 01:24:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,14 @@ for (let i = 0; i < botoes2.length; i++) {
         textos2[i].classList.add("ativo");
     }
 }''
+
+// Seleciona todos os elementos com a classe "card"
+const cards = document.querySelectorAll('.card');
+
+// Adiciona um evento de clique para cada card
+cards.forEach(card => {
+    card.addEventListener('click', function () {
+        // Ao clicar, adiciona ou remove a classe "flip" para virar o card
+        card.querySelector('.card-inner').classList.toggle('flip');
+    });
+});
